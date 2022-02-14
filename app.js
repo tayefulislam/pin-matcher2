df2c3a55de75ccefe056141b828f72ae4982593c
tayeful912 <kazitayefulislam12@gmail.com>
2022-02-14 13:28:26 +0600

    generate pin and typed pin complete

---
+++ app.js
@@ -0,0 +1,41 @@
+function getPin() {
+    let randomPin = Math.round(Math.random() * 10000)
+    let pinString = randomPin + '';
+
+    if (pinString.length == 4) {
+        return randomPin;
+    }
+    else {
+        console.log(pinString.length, 'disit pin', randomPin);
+        return getPin();
+    }
+}
+function generatePin() {
+    let getGenerateInput = document.getElementById('genarate-input');
+    getGenerateInput.value = getPin();
+}
+
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('display-input');
+    if (isNaN(number)) {
+        // isNaN mane hochce ei number chara onno kichu thakle if ar modde hochche ashbe
+
+        if (number == 'C') {
+            // isNaN condition true howar por if modde ashle and number == C hoi mane C te click korle sob claer hoy jabe
+
+            calcInput.value = '';
+        }
+    }
+
+    else {
+
+        const calcInput = document.getElementById('display-input');
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+
+})
